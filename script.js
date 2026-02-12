4ddc3ad2aa71eef5c93dceca2878a83fbeb57e0e
Shadowfax71 <252891906+Shadowfax71@users.noreply.github.com>
2026-02-12 16:53:37 +0100

    Make hover effect.
    Hovering works, will now work on the button to make a new grid

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ const gridSize = 16;
 for (let i = 0; i < gridSize * gridSize; i++) { 
     const square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mouseover', function() {
+        this.style.backgroundColor = 'black';
+    });
     container.appendChild(square);
+
 }
